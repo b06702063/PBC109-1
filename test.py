3ff5412608b28a097d6b06aa582dd050414983e6
b06702063 <75714566+b06702063@users.noreply.github.com>
2020-12-17 14:18:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-print('hello')
+import requests
+from bs4 import BeautifulSoup
+response = requests.get("https://www.costco.com.tw/Food/c/C8")
+soup = BeautifulSoup(response.text, "html.parser")
+print(soup)
