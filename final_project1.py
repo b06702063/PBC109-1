fc1c3896022e60957150eae44c6e9779818e9738
Shauni1118 <75960803+Shauni1118@users.noreply.github.com>
2020-12-21 13:18:32 +0800

    Add files via upload

---
+++ final_project1.py
@@ -0,0 +1,35 @@
+# _*_coding:utf8_*_
+
+import requests
+from bs4 import BeautifulSoup
+
+enter_name = input('請輸入食材:')
+url = 'https://online.carrefour.com.tw/tw/search?key='  # 抓網址
+url += enter_name
+url += '&categoryId='
+r = requests.get(url)  # 把網頁的原始碼抓下來
+soup = BeautifulSoup(r.text, 'html.parser')
+
+
+attr = {'type':"text/javascript"}
+product_tags = soup.find_all('script', attrs = attr)
+
+useful_inf = str(product_tags[12]).split(',')
+
+product = []
+price = []
+
+name = ()
+for i in useful_inf:
+	if 'EecProductName' in i:
+		name = i.strip('\\"')
+		name = name.strip('EecProductName\\":\\"')
+		product.append(name)
+for i in useful_inf:
+	if 'RealPrice' in i:
+		name = i.strip('\\"')
+		name = name.strip('RealPrice\\":\\"')
+		price.append(int(name))
+
+product_price = zip(product, price)
+print(dict(product_price))
