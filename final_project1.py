f661d6d37dd59294a99e49495a0288bbb3b7982b
b06702063 <75714566+b06702063@users.noreply.github.com>
2020-12-22 13:25:41 +0800

    Update final_project1.py

--- final_project1.py
+++ final_project1.py
@@ -3,7 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-enter_name = input('請輸入食材:')
+enter_name = input('請輸入食材:')  # 食材種類
 url = 'https://online.carrefour.com.tw/tw/search?key='  # 抓網址
 url += enter_name
 url += '&categoryId='
@@ -11,25 +11,37 @@ r = requests.get(url)  # 把網頁的原始碼抓下來
 soup = BeautifulSoup(r.text, 'html.parser')
 
 
-attr = {'type':"text/javascript"}
-product_tags = soup.find_all('script', attrs = attr)  # 找到<script type = text/javascript>這個標籤
+attr = {'type': "text/javascript"}
+# 找到<script type = text/javascript>這個標籤
+product_tags = soup.find_all('script', attrs=attr)
 
-useful_inf = str(product_tags[12]).split(',')  # 我研究完家樂福的網頁原始碼，發現需要的訊息是在第13個標籤裡，存成列表
+useful_inf = str(product_tags[12]).split(
+    ',')  # 我研究完家樂福的網頁原始碼，發現需要的訊息是在第13個標籤裡，存成列表
 
 product = []
 price = []
 
 name = ()
 for i in useful_inf:
-	if 'EecProductName' in i:  # 這個後面是品項名，所以我要找出列表中含EecProductName的
-		name = i.strip('\\"')  # 把頭尾雜雜的字元去掉
-		name = name.strip('EecProductName\\":\\"')  # 去掉這串字
-		product.append(name)  # 存成品項名字的列表
+    if 'EecProductName' in i:  # 這個後面是品項名，所以我要找出列表中含EecProductName的
+        name = i.strip('\\"')  # 把頭尾雜雜的字元去掉
+        name = name.strip('EecProductName\\":\\"')  # 去掉這串字
+        product.append(name)  # 存成品項名字的列表
 for i in useful_inf:
-	if 'RealPrice' in i:
-		name = i.strip('\\"')
-		name = name.strip('RealPrice\\":\\"')
-		price.append(int(name))
+    if 'RealPrice' in i:
+        name = i.strip('\\"')
+        name = name.strip('RealPrice\\":\\"')
+        price.append(int(name))
 
 product_price = zip(product, price)
 print(dict(product_price))
+
+
+'''costco'''
+
+cos_url = 'https://www.costco.com.tw/search?text='  # 抓網址
+cos_url += enter_name
+cos_r = requests.get(cos_url)  # 抓下原始碼
+cos_soup = BeautifulSoup(cos_r.text, 'html.parser')
+
+'''商品資訊存在gtm_products的list中'''
